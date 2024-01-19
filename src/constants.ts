998063131f2f19d5bd035f39090e136b74d34158
existenz31 <spaltrie1@gmail.com>
2024-01-19 12:02:19 +0100

    use the curated index instead of simon edit

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@
 //   "autocomplete_demo_products_query_suggestions";
 // export const ALGOLIA_ARTICLES_INDEX_NAME = "autocomplete_demo_articles";
 // export const ALGOLIA_FAQ_INDEX_NAME = "autocomplete_demo_faq";
-export const ALGOLIA_PRODUCTS_INDEX_NAME = "giant_bikes_simon_edit"; //"giant_bikes_curated";
+export const ALGOLIA_PRODUCTS_INDEX_NAME = "giant_bikes_curated"; //"giant_bikes_simon_edit";
 export const ALGOLIA_PRODUCTS_QUERY_SUGGESTIONS_INDEX_NAME =
   "giant_bikes_curated_query_suggestions";
 export const ALGOLIA_ARTICLES_INDEX_NAME = "giant_blog";
